cb462eb7ffd6a0a2f2dbbf1a2eb108bbe9b21afb
renatakaren <renatakarenr@gmail.com>
2025-09-21 20:20:20 -0300

    TDD feito

---
+++ src/CpfValidator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+public class CpfValidator
+{
+    public static bool IsValid(string cpf)
+    {
+        if (string.IsNullOrWhiteSpace(cpf) || cpf.Length != 11 || cpf.Distinct().Count() == 1)
+            return false;
+
+        int[] multiplicador1 = new int[9] {10, 9, 8, 7, 6, 5, 4, 3, 2};
+        int[] multiplicador2 = new int[10] {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
+
+        string tempCpf = cpf.Substring(0, 9);
+        int soma = 0;
+
+        for (int i = 0; i < 9; i++)
+            soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+
+        int resto = soma % 11;
+        if (resto < 2)
+            resto = 0;
+        else
+            resto = 11 - resto;
+
+        string digito = resto.ToString();
+        tempCpf += digito;
+        soma = 0;
+
+        for (int i = 0; i < 10; i++)
+            soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+
+        resto = soma % 11;
+        if (resto < 2)
+            resto = 0;
+        else
+            resto = 11 - resto;
+
+        digito += resto.ToString();
+        return cpf.EndsWith(digito);
+    }
+}
---
+++ tests/CpfValidatorTests.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+public class CpfValidatorTests
+{
+    [Fact]
+    public void CpfValido_DeveRetornarTrue()
+    {
+        string cpf = "52998224725";
+        Assert.True(CpfValidator.IsValid(cpf));
+    }
+
+    [Fact]
+    public void CpfInvalido_DeveRetornarFalse()
+    {
+        string cpf = "12345678900";
+        Assert.False(CpfValidator.IsValid(cpf));
+    }
+
+    [Fact]
+    public void CpfComLetras_DeveRetornarFalse()
+    {
+        string cpf = "abc98224725";
+        Assert.False(CpfValidator.IsValid(cpf));
+    }
+
+    [Fact]
+    public void CpfComMenosDe11Digitos_DeveRetornarFalse()
+    {
+        string cpf = "12345678";
+        Assert.False(CpfValidator.IsValid(cpf));
+    }
+
+    [Fact]
+    public void CpfComTodosDigitosIguais_DeveRetornarFalse()
+    {
+        string cpf = "11111111111";
+        Assert.False(CpfValidator.IsValid(cpf));
+    }
+}
